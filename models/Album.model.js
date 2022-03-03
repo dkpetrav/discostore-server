8bdc58f1c3bcfaf7213e3f89c8a472e6915f9164
Peter Ravnsbæk <dkpetrav@lego.com>
2022-03-03 09:20:56 +0100

    initial model description

---
+++ models/Album.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const albumSchema = new Schema(
+  {
+    title: { type: String },
+    artist: { type: String },
+    genre: { type: String },
+    price: { type: String },
+    imageURL: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+module.exports = model('Album', userSchema)
---
+++ models/Order.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const orderSchema = new Schema({
+  orderNumber: { type: Number },
+  customerId: { type: Number },
+  username: { type: String },
+  street: { type: String, required: true },
+  streetNumber: { type: Number, required: true },
+  city: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  country: { type: String },
+  //orderItem(s):
+  albumId: { type: String },
+  quantity: { type: Number },
+})
+
+module.exports = model('Order', userSchema)
