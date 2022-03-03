ebdec998f9090c433e9b519655a9b8268b7cf80d
Peter Ravnsbæk <dkpetrav@lego.com>
2022-03-03 11:35:55 +0100

    app extended

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ require('./config')(app)
 const allRoutes = require('./routes/index.routes')
 app.use('/api', allRoutes)
 
+const albumRouter = require('./routes/album.routes'); 
+app.use('/api', albumRouter);
+
+const orderRouter = require('./routes/order.routes'); 
+app.use('/api', orderRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
 
