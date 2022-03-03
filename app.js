14d4fc7f35a4c228fb23cb227c0d0758c6ba42d1
Peter Ravnsbæk <dkpetrav@lego.com>
2022-03-03 16:06:01 +0100

    routes working

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.use('/api', albumRouter)
 const orderRouter = require('./routes/order.routes')
 app.use('/api', orderRouter)
 
+const userRouter = require('./routes/user.routes')
+app.use('/api', userRouter)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
 
