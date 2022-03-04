e8618f52c3913e4cc7157015670f16881c891c5a
Peter Ravnsbaek <84021050+dkpetrav@users.noreply.github.com>
2022-03-04 09:11:14 +0100

    seeds injected (#11)
    Co-authored-by: Peter Ravnsbæk <dkpetrav@lego.com>

--- models/Order.model.js
+++ models/Order.model.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
-const orderSchema = new Schema({
-  //customerId: { type: Number },
-  username: { type: String },
-  street: { type: String, required: true },
-  streetNumber: { type: Number, required: true },
-  city: { type: String, required: true },
-  zipCode: { type: String, required: true },
-  country: { type: String },
-  //orderItem(s):
-  albumId: { type: String },
-  quantity: { type: Number },
-})
+const orderSchema = new Schema(
+  {
+    //customerId: { type: Number },
+    username: { type: String },
+    street: { type: String, required: true },
+    streetNumber: { type: Number, required: true },
+    city: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    country: { type: String },
+    //orderItem(s):
+    albumId: { type: String },
+    quantity: { type: Number },
+  },
+  {
+    timestamps: true,
+  },
+)
 
 module.exports = model('Order', orderSchema)
